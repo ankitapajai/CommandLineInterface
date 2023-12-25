27f3b6e278b8726010708844388b90fcc1c17842
ankitapajai <ankitapajai@gmail.com>
2023-12-25 12:33:21 +0530

    -

---
+++ src/main/java/com/bootcoding/UserCommandApp/store/InMemoryStore.java
@@ -0,0 +1,10 @@
+package com.bootcoding.UserCommandApp.store;
+
+import com.bootcoding.UserCommandApp.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemoryStore {
+    public static List<User> users = new ArrayList<>();
+}
